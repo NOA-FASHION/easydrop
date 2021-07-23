ac980eed9b163402d103ae5093dd5a9b7476497a
malmont <michel.almont@gmail.com>
2021-07-22 21:36:18 -0400

    _initProduitGagnantList

--- lib/controllers/challenge_controller.dart
+++ lib/controllers/challenge_controller.dart
@@ -1,5 +1,33 @@
+import 'dart:convert';
+
+import 'package:easydrop/models/drop_models.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String keyAcces = "challengeList";
 
 class Challengecontroller extends ChangeNotifier {
-  
+  late SharedPreferences _localData;
+  Challengecontroller() {
+    _initProduitGagnantList();
+  }
+
+  void _initProduitGagnantList() async {
+    _localData = await SharedPreferences.getInstance();
+    List<ProduitGagnant> _listProduitGagnant = [];
+    List<Map<String, dynamic>> _jsonDecodeList;
+    final List<String>? _tempList = _localData.getStringList(keyAcces);
+    if (_tempList != null) {
+      _jsonDecodeList = _tempList
+          .map((challengeEncoded) => jsonDecode(challengeEncoded))
+          .toList()
+          .cast<Map<String, dynamic>>();
+
+      _listProduitGagnant = _jsonDecodeList
+          .map((challenge) => ProduitGagnant.fromJSON(challenge))
+          .toList();
+    }
+
+    notifyListeners();
+  }
 }
